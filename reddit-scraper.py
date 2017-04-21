059b639f91693efae39024dd6703dc02e83e0334
imwillson <liwillson1@gmail.com>
2017-04-20 20:20:07 -0400

    fix URL by adding https

--- reddit-scraper.py
+++ reddit-scraper.py
@@ -2,7 +2,7 @@ from urllib2 import Request, urlopen
 from time import sleep
 import json
 
-_URL = 'http://www.reddit.com/user/Youreallyfat/comments/.json'
+_URL = 'https://www.reddit.com/user/Yourereallyfat/comments'
 _headers = {'User-Agent': 'reddit-scraper v1.0 (by /u/Yourereallyfat )'}
 
 # # holds the URL in a Request object
@@ -16,20 +16,31 @@ def get_comments(URL,head,delay=2):
     # suspends the current thread for (seconds)
     # ensure we don't GET too frequently or the API will block us
     sleep(delay) 
-    request = Request(URL, "", head)
+    request = Request(URL, headers=head)
+
+    # test print the dictionary
+    # for key,value in head.iteritems():
+    # 	print key,value
+
+
+
     try:
         response = urlopen(request)
+        print("response was a sucess")
         data = response.read()
     except:
         sleep(delay+5)
         response = urlopen(request)
         data = response.read()
+
+        print("error in function")
     return data
 
 
 
 
 def main():
+
 	try:	
 		x = get_comments(_URL,_headers)
 		print(x)
